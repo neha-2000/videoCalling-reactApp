cfb21f60838c0277d1f347ebbd6e2bf021f43dd2
neha-2000 <bhaleraonehali7@gmail.com>
2021-07-25 12:12:57 +0530

    nine commit

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -1,20 +1,20 @@
 import React  from 'react'
-//import { useContext } from 'react'
-//import { Button } from '@material-ui/core';
-//import { SocketContext } from "../SocketContext";
+import { useContext } from 'react'
+import { Button } from '@material-ui/core';
+import { SocketContext } from "../SocketContext";
 
 const Notifications = () => {
-    // const {answerCall, call, callAccepted } = useContext(SocketContext);
+ const {answerCall, call, callAccepted } = useContext(SocketContext);
     return (
         <div>
             <h1>Notification display</h1>
-             {/* {call.isReceivingCall && !callAccepted  &&(
+             {call.isReceivingCall && !callAccepted  &&(
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <h1>{call.name} is calling:</h1>
                     <Button variant="contained" color="primary" onClick={answerCall} >Answer..</Button>
                 </div>
 
-            )}  */}
+            )} 
         </div>
     );
 };
--- src/SocketContext.js
+++ src/SocketContext.js
@@ -24,6 +24,7 @@ const ContextProvider =({children}) => {
         .then((currentStream) =>{
             setStream(currentStream);
             myVideo.current.srcObject=currentStream;
+            
         })
         socket.on('me',(id)=>setMe(id));
 
