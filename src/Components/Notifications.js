40ee74a39d54aec0d6a5d08af66c6ab040db5a25
neha-2000 <bhaleraonehali7@gmail.com>
2021-07-25 10:12:37 +0530

    six commit

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -7,13 +7,13 @@ const Notifications = () => {
     return (
         <div>
             <h1>Notification display</h1>
-             {call.isReceivingCall && !callAccepted  &&(
+             {/* {call.isReceivingCall && !callAccepted  &&(
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <h1>{call.name} is calling:</h1>
                     <Button variant="contained" color="primary" onClick={answerCall} >Answer..</Button>
                 </div>
 
-            )} 
+            )}  */}
         </div>
     );
 };
